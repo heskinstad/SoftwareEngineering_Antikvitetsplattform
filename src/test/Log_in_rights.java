92e9c75e957b621ceb0568c90c477d7878dac7cf
Marius <mariug@hiof.no>
2021-09-29 14:45:25 +0200

    Justeringer på filnavn og fxml vindu.

---
+++ src/test/Log_in_rights.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Log_in_rights {
+
+    @Test
+    public void Check_if_admin_rights() {
+
+        fail("We dont have any code");
+
+    }
+}
