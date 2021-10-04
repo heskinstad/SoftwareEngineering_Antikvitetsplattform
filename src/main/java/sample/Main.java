252ff6ff2e430d2b442939a30f466ee9ca0c14bf
SigurdVaagane <sigurdva@hiof.no>
2021-10-04 10:56:09 +0200

    new file butikk.fxml

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("butikkView.fxml"));
         primaryStage.setTitle("Antikvitets platform");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
     }
 
