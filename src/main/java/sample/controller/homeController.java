a394390eb71702390060a071bb5797f6cf4a5e47
Hans Skinstad <43535291+heskinstad@users.noreply.github.com>
2021-10-04 11:54:29 +0200

    funksjonalitet for admin login lagt til

--- src/main/java/sample/controller/homeController.java
+++ src/main/java/sample/controller/homeController.java
@@ -1,4 +1,40 @@
 package sample.controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class homeController {
+
+    @FXML
+    private Button btnLoginAdmin;
+
+    @FXML
+    public void initialize() {
+        this.btnLoginAdmin.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                Parent root;
+                try {
+                    root = FXMLLoader.load(getClass().getResource("../view/admin.fxml"));
+                    Stage stage = new Stage();
+                    stage.setTitle("Admin Control");
+                    stage.setScene(new Scene(root, 400, 600));
+                    stage.show();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+
+
 }
