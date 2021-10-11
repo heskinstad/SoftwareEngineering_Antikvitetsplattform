01e85a97c77524d1883ae822cc98d4578d759129
Hans Skinstad <43535291+heskinstad@users.noreply.github.com>
2021-10-11 11:07:52 +0200

    Jobber med DataHandler_varer

--- src/main/java/sample/data/DataHandler_varer.java
+++ src/main/java/sample/data/DataHandler_varer.java
@@ -1,4 +1,28 @@
 package sample.data;
 
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import sample.model.Vare;
+
+import java.io.FileReader;
+import java.nio.file.Paths;
+
+
 public class DataHandler_varer {
+
+    public static void main(String[] args) throws Exception {
+        try {
+            // create object mapper instance
+            ObjectMapper mapper = new ObjectMapper();
+
+            // convert JSON string to Vare object
+            Vare vare = mapper.readValue(Paths.get("../../resources/JSON/varer.JSON").toFile(), Vare.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.getMessage();
+        }
+    }
+
 }
