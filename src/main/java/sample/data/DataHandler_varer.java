90e37ebd0b58f7795d44e0531913a256cacf8e5b
Hans Skinstad <43535291+heskinstad@users.noreply.github.com>
2021-10-11 11:46:26 +0200

    Fiksa DataHandler_varer

--- src/main/java/sample/data/DataHandler_varer.java
+++ src/main/java/sample/data/DataHandler_varer.java
@@ -1,7 +1,5 @@
 package sample.data;
 
-
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import sample.model.Vare;
 
@@ -17,11 +15,10 @@ public class DataHandler_varer {
             ObjectMapper mapper = new ObjectMapper();
 
             // convert JSON string to Vare object
-            Vare vare = mapper.readValue(Paths.get("../../resources/JSON/varer.JSON").toFile(), Vare.class);
-        } catch (Exception e) {
+            Vare vare = mapper.readValue(Paths.get("../../../resources/JSON/varer.JSON").toFile(), Vare.class);
+        }
+        catch (Exception e) {
             e.printStackTrace();
-        } catch (JsonMappingException e) {
-            e.getMessage();
         }
     }
 
