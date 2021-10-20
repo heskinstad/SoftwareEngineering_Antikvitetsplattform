63e975dd811ee7c3f0c8972c0d7189463671af48
Hans Skinstad <43535291+heskinstad@users.noreply.github.com>
2021-10-20 15:09:03 +0200

    Fikset onAction-knapper

--- src/main/java/sample/controller/listWindowController.java
+++ src/main/java/sample/controller/listWindowController.java
@@ -1,7 +1,9 @@
 package sample.controller;
 
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import sample.data.DataHandler;
 
@@ -13,6 +15,14 @@ public class listWindowController {
     @FXML
     public void initialize() {
         //tableList.setItems((ObservableList) DataHandler.lastInnKlage("src/main/resources/JSON/klager.JSON"));
+    }
+
+    public void refresh(ActionEvent actionEvent) {
+
+    }
+
+    public void changeUser(ActionEvent actionEvent) {
+
     }
 
 
