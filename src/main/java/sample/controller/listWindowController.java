2ac858d7361fdca1bae8d06370ed6897d00a4439
Hans Skinstad <43535291+heskinstad@users.noreply.github.com>
2021-10-20 22:50:11 +0200

    Liste for visning av klager fungerer!

--- src/main/java/sample/controller/listWindowController.java
+++ src/main/java/sample/controller/listWindowController.java
@@ -1,32 +1,49 @@
 package sample.controller;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import sample.data.DataHandler;
+import sample.model.Klage;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class listWindowController {
 
-    @FXML
-    TableView table_SaleHistory;
-    @FXML
-    TableView table_Complaints;
+    @FXML TableView table_SaleHistory;
+    @FXML TableView<Klage> table_Complaints;
+    @FXML TableColumn<Klage,Integer> id;
+    @FXML TableColumn<Klage,String> innsender;
+    @FXML TableColumn<Klage,String> melding;
+    @FXML TableColumn<Klage,String> butikk;
+    @FXML TableColumn<Klage,LocalDateTime> tidspunkt;
 
     @FXML
     public void initialize() {
-        table_Complaints.setItems((ObservableList) DataHandler.lastInnKlager("src/main/resources/JSON/klager.JSON"));
+        refreshKlageListe();
     }
 
     public void refresh(ActionEvent actionEvent) {
-        table_Complaints.setItems((ObservableList) DataHandler.lastInnKlager("src/main/resources/JSON/klager.JSON"));
+        refreshKlageListe();
     }
 
     public void changeUser(ActionEvent actionEvent) {
 
     }
 
-
+    private void refreshKlageListe() {
+        ArrayList<Klage> klager = new ArrayList<Klage>();
+        klager.addAll(DataHandler.lastInnKlagerV2("/src/main/resources/JSON/klager.JSON"));
+        ObservableList<Klage> klagerObservable = FXCollections.observableArrayList(klager);
+        table_Complaints.setItems(klagerObservable);
+    }
 
 }
--- src/main/java/sample/data/DataHandler.java
+++ src/main/java/sample/data/DataHandler.java
@@ -1,26 +1,18 @@
 package sample.data;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.ObservableList;
 import sample.model.Klage;
 import sample.model.Vare;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class DataHandler {
-
-    /*public static void main(String[] args) throws Exception {
-        try {
-            // create object mapper instance
-            ObjectMapper mapper = new ObjectMapper();
-
-            // convert JSON string to Vare object
-            Vare vare = mapper.readValue(Paths.get("../../../resources/JSON/varer.JSON").toFile(), Vare.class);
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
 
     public static Vare lastInnVare(String localPath) {
         try {
@@ -70,18 +62,20 @@ public class DataHandler {
         }
     }
 
-    public static Klage lastInnKlager(String localPath) {
+    // OLD, SOON TO BE DELETED
+    public static ArrayList<Klage> lastInnKlager(String localPath) {
         try {
             // create object mapper instance
             ObjectMapper mapper = new ObjectMapper();
             mapper.findAndRegisterModules();
 
-            // convert JSON string to Klage object
+            // convert JSON string to a list of Klage objects
             String path = new File("").getAbsolutePath() + localPath;
             System.out.println("Laster inn fra " + path);
-            Klage klage = mapper.readValue(new FileReader(path), Klage.class);
+            ArrayList<Klage> klager = new ArrayList<>(Arrays.asList(mapper.readValue(new FileReader(path), Klage.class)));
 
-            return klage;
+            return klager;
+
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -89,4 +83,17 @@ public class DataHandler {
         return null;
     }
 
+    public static ArrayList<Klage> lastInnKlagerV2(String localPath) {
+        try {
+            final ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.findAndRegisterModules();
+            String path = new File("").getAbsolutePath() + localPath;
+            ArrayList<Klage> klager = objectMapper.readValue(new File(path), new TypeReference<List<Klage>>(){});
+            return klager;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
