a299225c1d33fd1e8018cb33bb4b7e384f5e8df4
SigurdVaagane <sigurdva@hiof.no>
2021-10-25 11:48:20 +0200

    jobbet med legge til varer i view

--- src/main/java/sample/controller/butikkController.java
+++ src/main/java/sample/controller/butikkController.java
@@ -5,9 +5,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,12 +19,30 @@ public class butikkController extends homeController {
 
     private boolean isEditDesc = false; // verdi for å sjekke om editMode på beskrivelse er aktivt
 
+    @FXML Text vare_tittel_0;
+    @FXML Text vare_tittel_1;
+    @FXML Text vare_tittel_2;
+    @FXML Text vare_tittel_3;
+    @FXML Text vare_beskrivelse_0;
+    @FXML Text vare_beskrivelse_1;
+    @FXML Text vare_beskrivelse_2;
+    @FXML Text vare_beskrivelse_3;
+    @FXML ImageView vare_url_0;
+    @FXML ImageView vare_url_1;
+    @FXML ImageView vare_url_2;
+    @FXML ImageView vare_url_3;
+
+
     @FXML
     public void initialize(ActionEvent actionEvent) {
+        refreshVarer();
+    }
+
+    private void refreshVarer() {
     }
 
     public void editDesc(ActionEvent actionEvent){
-        //TODO need model to finnish buttonEvent, need model
+
 
         Scene scene = ((Node) actionEvent.getSource()).getScene();
         Button btnEditDesc = (Button) scene.lookup("#btnEditDesc");
@@ -35,6 +56,9 @@ public class butikkController extends homeController {
             btnEditDesc.setText("edit");
             txtDesc.setText(textTemp);
             txtDesc.setEditable(false);
+
+            Alert alert = new Alert(Alert.AlertType.WARNING, "Beskrivelse forandret!");
+            alert.showAndWait();
         }
         isEditDesc ^= true;
     }
