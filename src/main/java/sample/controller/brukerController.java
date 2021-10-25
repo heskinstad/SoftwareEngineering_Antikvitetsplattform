d7ab5224e674634a63cf9bc7dfecafdafbd13109
vetle_93dri9c <vetle.elnes@icloud.com>
2021-10-25 09:49:11 +0200

    Fikse så man kan legge til flere varer.

--- src/main/java/sample/controller/brukerController.java
+++ src/main/java/sample/controller/brukerController.java
@@ -1,33 +1,32 @@
 package sample.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import sample.model.Klage;
 import sample.model.Vare;
 import sample.data.DataHandler;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 public class brukerController extends homeController {
 
-    @FXML
-    public Button btnLoginAdmin;
-    @FXML
-    public TextField varenavn;
-    @FXML
-    public TextArea vareBeskrivelse;
+    @FXML public Button btnLoginAdmin;
+    @FXML public TextField varenavn;
+    @FXML public TextArea vareBeskrivelse;
+
 
     @FXML
     public void initialize() {
-        //Vare vare = DataHandler_varer.lastInnVare("/src/main/java/resources/JSON/varer.JSON");
+        //Vare vare = DataHandler.lastInnVare("/src/main/java/resources/JSON/varer.JSON");
         //varenavn.setText(vare.getNavn());
         //vareBeskrivelse.setText(vare.getBeskrivelse());
 
-        //DataHandler.leggInnVare(new Vare(3, "test2", "bla bla bla...", "ikke helt beste butikken...", 1, LocalDateTime.now(), "aaa"), "/src/main/resources/JSON/varer.JSON");
-        //DataHandler.leggInnKlage(new Klage(3, "Thomas", "Dette fungerte ikke", "Heiabutikken", LocalDateTime.now()), "/src/main/resources/JSON/klager.JSON");
+        //DataHandler.leggInnVare(new Vare(3, "Mango", "bla bla bla...", "ikke helt beste butikken...", 1, LocalDateTime.now(), "aaa"), "/src/main/resources/JSON/varer.JSON");
+        //DataHandler.leggInnKlage(new Klage(3, "Per", "Dette fungerte ikke", "Heiabutikken", LocalDateTime.now()), "/src/main/resources/JSON/klager.JSON");
     }
 
     public void kjopVare1(ActionEvent actionEvent) {
@@ -49,4 +48,6 @@ public class brukerController extends homeController {
     public void aapneKlageSkjema(ActionEvent actionEvent) {
         openNewInterface(actionEvent, "../view/klage.fxml", "Klageskjema", 600, 310);
     }
+
+
 }
