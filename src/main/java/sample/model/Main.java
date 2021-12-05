b235a61becb79d02a579219c511e3efccb244490
Hans Skinstad <heskinstad@gmail.com>
2021-12-05 20:40:45 +0100

    minor skriveleif

--- src/main/java/sample/model/Main.java
+++ src/main/java/sample/model/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/homeView.fxml"));
-        primaryStage.setTitle("Antikvitetsplatform");
+        primaryStage.setTitle("Antikvitetsplattform");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
