5bd21feb4e5841de14633a598e880bd954121240
SigurdVaagane <sigurdva@hiof.no>
2021-12-05 12:58:07 +0100

    lagd ny test

--- src/main/java/sample/util/AntikkUtil.java
+++ src/main/java/sample/util/AntikkUtil.java
@@ -1,5 +1,6 @@
 package sample.util;
 
+
 public class AntikkUtil {
     public static int getTrueVareArrayStartIndex(int side, int numberOfElements){
         return (side - 1) * numberOfElements;
---
+++ src/test/java/Test_utility.java
@@ -0,0 +1,22 @@
+import sample.util.AntikkUtil;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Test_utility {
+
+    @Test
+    public void Test_get_index() {
+        assertEquals(0, AntikkUtil.getTrueVareArrayStartIndex(1, 1));
+        assertEquals(90, AntikkUtil.getTrueVareArrayStartIndex(10, 10));
+        assertEquals(10, AntikkUtil.getTrueVareArrayStartIndex(3, 5));
+        assertEquals(44, AntikkUtil.getTrueVareArrayStartIndex(5, 11));
+        assertEquals(42, AntikkUtil.getTrueVareArrayStartIndex(3, 21));
+        assertEquals(32, AntikkUtil.getTrueVareArrayStartIndex(2, 32));
+        assertEquals(32, AntikkUtil.getTrueVareArrayStartIndex(3, 16));
+        assertEquals(32, AntikkUtil.getTrueVareArrayStartIndex(5, 8));
+        assertEquals(32, AntikkUtil.getTrueVareArrayStartIndex(9, 4));
+        assertEquals(32, AntikkUtil.getTrueVareArrayStartIndex(17, 2));
+        assertEquals(32, AntikkUtil.getTrueVareArrayStartIndex(33, 1));
+
+    }
+}
