e88b7c13bf430584193c6ccfb52e59c3bb7d3cbe
SigurdVaagane <sigurdva@hiof.no>
2021-12-06 13:42:09 +0100

    ny funksjon i AntikkUtil

--- src/main/java/sample/util/AntikkUtil.java
+++ src/main/java/sample/util/AntikkUtil.java
@@ -1,8 +1,23 @@
 package sample.util;
 
 
+import sample.model.Vare;
+
+import java.util.ArrayList;
+
 public class AntikkUtil {
     public static int getTrueVareArrayStartIndex(int side, int numberOfElements){
         return (side - 1) * numberOfElements;
+    }
+
+    public static ArrayList<Vare> GetVareMedButikkNavn(ArrayList<Vare> vareListe, String butikkNavn){
+
+        for (int i = 0; i < vareListe.size(); i++) {
+            if (!vareListe.get(i).getButikk().equals(butikkNavn)) {
+                vareListe.remove(i);
+                i--;
+            }
+        }
+        return vareListe;
     }
 }
